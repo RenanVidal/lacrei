6dbb59fde4606cc5e191a06626ea17099ac96ec3
Renan Vidal <rvidal.rvr@gmail.com>
2023-06-01 23:12:54 -0300

    Lacrei - Feat: Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className='Header'>
+        <a href="/">Lacrei</a>
+        <nav>
+            <a href="/" className='active'>Home</a>
+            <a href="/pessoa-usuaria">Pessoa Usária</a>
+            <a href="profissional">Profissional</a>
+        </nav>
+    </header>
+  );
+}
